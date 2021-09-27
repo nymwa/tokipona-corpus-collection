46668905fac464570b6cc44c2c92417ebf72e2de
nymwa <nymwa0@gmail.com>
2021-09-28 04:37:31 +0900

    init

---
+++ tokipona1000/init_conll.py
@@ -0,0 +1,21 @@
+from ilonimi.normalizer import Normalizer
+from ilonimi.tokenizer import Tokenizer
+
+def main():
+    normalizer = Normalizer()
+    tokenizer = Tokenizer()
+
+    with open('tokipona1000.txt') as f:
+        for i, x in enumerate(f):
+            x = x.strip()
+            x = normalizer(x)
+            x = tokenizer(x)
+            print('# {}: {}'.format(i + 1, x))
+            for k, w in enumerate(x.split()):
+                print('{}	{}	{}	{}	{}'.format(k + 1, w, 'X', '0', 'x'))
+            print('')
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ tokipona1000/vis.py
@@ -0,0 +1,133 @@
+head = '''
+<html>
+<head>
+   <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+   <title>Toki Pona UD Visualization</title>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+   <link rel="stylesheet" type="text/css" href="https://universaldependencies.org/css/jquery-ui-redmond.css"/>
+   <link rel="stylesheet" type="text/css" href="https://universaldependencies.org/css/style.css"/>
+   <link rel="stylesheet" type="text/css" href="https://universaldependencies.org/css/style-vis.css"/>
+   <script type="text/javascript" src="https://universaldependencies.org/lib/ext/head.load.min.js"></script>
+</head>
+<body>
+<div id="main" class="center">
+<div id="content">
+<script type="text/javascript">
+   console.time('loading libraries');
+   var root = 'https://universaldependencies.org/'; // filled in by jekyll
+   head.js(
+                     // External libraries
+                     // DZ: Copied from embedding.html. I don't know which one is needed for what, so I'm currently keeping them all.
+                     root + 'lib/ext/jquery.min.js',
+                     root + 'lib/ext/jquery.svg.min.js',
+                     root + 'lib/ext/jquery.svgdom.min.js',
+                     root + 'lib/ext/jquery.timeago.js',
+                     root + 'lib/ext/jquery-ui.min.js',
+                     root + 'lib/ext/waypoints.min.js',
+                     root + 'lib/ext/jquery.address.min.js'
+                  );
+</script>
+'''
+
+tail = '''
+<!-- support for embedded visualizations -->
+<script type="text/javascript">
+   var root = 'https://universaldependencies.org/'; // filled in by jekyll
+   head.js(
+                  // We assume that external libraries such as jquery.min.js have already been loaded outside!
+                  // (See _layouts/base.html.)
+
+                  // brat helper modules
+                  root + 'lib/brat/configuration.js',
+                  root + 'lib/brat/util.js',
+                  root + 'lib/brat/annotation_log.js',
+                  root + 'lib/ext/webfont.js',
+                  // brat modules
+                  root + 'lib/brat/dispatcher.js',
+                  root + 'lib/brat/url_monitor.js',
+                  root + 'lib/brat/visualizer.js',
+
+                  // embedding configuration
+                  root + 'lib/local/config.js',
+                  // project-specific collection data
+                  root + 'lib/local/collections.js',
+
+                  // Annodoc
+                  root + 'lib/annodoc/annodoc.js',
+
+                  // NOTE: non-local libraries
+                  'https://spyysalo.github.io/conllu.js/conllu.js'
+               );
+
+   var webFontURLs = [
+                  //        root + 'static/fonts/Astloch-Bold.ttf',
+                  root + 'static/fonts/PT_Sans-Caption-Web-Regular.ttf',
+                  root + 'static/fonts/Liberation_Sans-Regular.ttf'
+               ];
+
+   var setupTimeago = function() {
+                  jQuery("time.timeago").timeago();
+               };
+
+   head.ready(function() {
+                  setupTimeago();
+
+                  // mark current collection (filled in by Jekyll)
+                  Collections.listing['_current'] = 'u-overview';
+
+                  // perform all embedding and support functions
+                  Annodoc.activate(Config.bratCollData, Collections.listing);
+               });
+</script>
+</div>
+</body>
+</html>
+'''
+
+
+def load_lipu(file):
+    lines = []
+    with open(file) as f:
+        for line in f:
+            if line.startswith('###'):
+                break
+            line = line.strip()
+            lines.append(line)
+    xs = '\n'.join(lines).split('\n\n')
+    xs = [x.strip() for x in xs]
+    return xs
+
+
+def to_conllu(lines, f):
+    print('0\tROOT\t_\t_\t_\t_\t0\troot', file = f)
+    for line in lines.split('\n'):
+        if line.startswith('#'):
+            continue
+        line = line.split('\t')
+        line = '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(
+                line[0],
+                line[1],
+                line[1],
+                line[2],
+                '_',
+                '_',
+                line[3],
+                line[4])
+        print(line, file = f)
+
+
+def main():
+    lipu = load_lipu('lipu.tsv')
+    for i in range(10):
+        sub = lipu[i * 100 : (i + 1) * 100]
+        with open('lipu.{}.html'.format(i + 1), 'w') as f:
+            print(head, file = f)
+            for x in sub:
+                print('<pre><code class="language-conllu">\n', file = f)
+                to_conllu(x, f)
+                print('</code></pre>\n', file = f)
+            print(tail, file = f)
+
+if __name__ == '__main__':
+    main()
+
