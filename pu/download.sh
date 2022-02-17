cb26ca4b07a0fa1f0a6dcb63818f6ef512333c5b
nymwa <nymwa0@gmail.com>
2022-02-17 17:26:44 +0900

    add

--- pu/download.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-wget https://gist.githubusercontent.com/increpare/10d94b6d7f223dd0f07c73b10dadc020/raw/982467ade9df81fd683062c2b4ddb6bf0013b3aa/toki%2520pona%2520corpus%2520sentences%2520+%2520paragraphs%2520from%2520pu
-mv 'toki pona corpus sentences + paragraphs from pu' pu.txt
-
